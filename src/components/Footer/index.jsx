fb32f99a330d358b63aa6b92cb252c8706de329b
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-11-06 21:21:55 -0300

    Added credits

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,8 +4,13 @@ import './styles.css';
 const Footer = () => {
 
     return (
-        <>    
-        </>
+        <footer className="footer">
+            <div className="content has-text-centered">
+                <p>
+                    Site desenvolvido por <a href="https://github.com/Giancarl021" target="_blank" rel="noopener noreferrer">Giancarlo Luz</a>.
+                </p>
+            </div>
+        </footer>
     );
 };
 
